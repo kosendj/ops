b41fda81cab8d6e4668c53fc68cafc541cf982c3
Shota Fukumori (sora_h) <her@sorah.jp>
2015-09-08 04:23:18 +0900

    Extract to role

--- itamae/hosts/box/retoree/default.rb
+++ itamae/hosts/box/retoree/default.rb
@@ -1,26 +1,6 @@
 include_cookbook 'base'
-include_cookbook 'iptables-persistent'
 include_cookbook 'nginx'
-include_cookbook 'dnsmasq'
 
-remote_file '/etc/dnsmasq.conf' do
-  owner 'root'
-  group 'root'
-  mode  '0644'
-end
-
-remote_file '/etc/iptables/rules.v4' do
-  owner 'root'
-  group 'root'
-  mode  '0644'
-end
-
-remote_file '/etc/sysctl.d/50-kosendj-router.conf' do
-  owner 'root'
-  group 'root'
-  mode  '0644'
-  notifies :run, 'execute[sysctl --system]'
-end
-
+include_role 'box-router'
 include_role 'gjcase_storage'
 include_role 'camo'
