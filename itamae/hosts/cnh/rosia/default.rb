ca7e2c2c226d71503ed3b6dfa88cb4074d84864c
Shota Fukumori (sora_h) <her@sorah.jp>
2015-09-10 00:42:53 +0900

    rosia: iptables

--- itamae/hosts/cnh/rosia/default.rb
+++ itamae/hosts/cnh/rosia/default.rb
@@ -1,2 +1,15 @@
 include_recipe '../base/default.rb'
 
+include_cookbook 'iptables-persistent'
+
+remote_file '/etc/iptables/rules.v4' do
+  owner 'root'
+  group 'root'
+  mode  '0644'
+end
+
+remote_file '/etc/iptables/rules.v6' do
+  owner 'root'
+  group 'root'
+  mode  '0644'
+end
