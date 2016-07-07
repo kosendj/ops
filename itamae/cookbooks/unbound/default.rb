4b5db84a0e06896215eeee0fb63b7d1cbc0a2af7
Sorah Fukumori <her@sorah.jp>
2016-07-08 06:39:31 +0900

    fix required file/directory modes

--- itamae/cookbooks/unbound/default.rb
+++ itamae/cookbooks/unbound/default.rb
@@ -11,6 +11,18 @@ execute 'unbound-anchor -v; unbound-anchor -v' do
   not_if 'test -e /etc/unbound/root.key'
 end
 
+directory '/etc/unbound' do
+  owner 'unbound'
+  group 'unbound'
+  mode  '0755'
+end
+
+file '/etc/unbound/root.key' do
+  owner 'unbound'
+  group 'unbound'
+  mode  '0644'
+end
+
 execute 'unbound-control-setup' do
   user 'root'
   command '/usr/sbin/unbound-control-setup -d /etc/unbound && chgrp unbound /etc/unbound/*.pem /etc/unbound/*.key && chmod 640 /etc/unbound/*.pem /etc/unbound/*.key'
