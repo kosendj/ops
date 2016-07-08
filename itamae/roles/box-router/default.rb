f5b50725a9bc75b37c4c361ee7df4c00f6f2dcc1
Sorah Fukumori <her@sorah.jp>
2016-07-09 03:15:27 +0900

    update dnsmasq configuration for current setup

--- itamae/roles/box-router/default.rb
+++ itamae/roles/box-router/default.rb
@@ -1,29 +1,7 @@
-include_cookbook 'iptables-persistent'
 include_cookbook 'dnsmasq'
 
 remote_file '/etc/dnsmasq.conf' do
   owner 'root'
   group 'root'
   mode  '0644'
-end
-
-remote_file '/etc/iptables/rules.v4' do
-  owner 'root'
-  group 'root'
-  mode  '0644'
-end
-
-remote_file '/etc/iptables/rules.v6' do
-  owner 'root'
-  group 'root'
-  mode  '0644'
-end
-
-include_cookbook 'sysctl-ipv4-forward'
-
-remote_file '/etc/sysctl.d/50-kosendj-router.conf' do
-  owner 'root'
-  group 'root'
-  mode  '0644'
-  notifies :run, 'execute[sysctl --system]'
 end
