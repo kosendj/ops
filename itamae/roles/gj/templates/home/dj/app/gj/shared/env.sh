13d39304242cff7a7078cbded2054be6d8fedb85
Sorah Fukumori <her@sorah.jp>
2016-07-08 23:00:42 +0900

    adopting https... :lock:

--- itamae/roles/gj/templates/home/dj/app/gj/shared/env.sh
+++ itamae/roles/gj/templates/home/dj/app/gj/shared/env.sh
@@ -7,7 +7,7 @@ export CONSUMER_KEY="<%= node[:secrets][:'gj-twitter-consumer_key'] %>"
 export CONSUMER_SECRET="<%= node[:secrets][:'gj-twitter-consumer_secret'] %>"
 export ACCESS_TOKEN="<%= node[:secrets][:'gj-twitter-access_token'] %>"
 export ACCESS_TOKEN_SECRET="<%= node[:secrets][:'gj-twitter-access_token_secret'] %>"
-export CAMO_URL=http://camo.kosendj-bu.in
+export CAMO_URL=https://camo.kosendj-bu.in
 export CAMO_KEY="<%= node[:secrets][:camo_key] %>"
 
 exec "$@"
--- itamae/roles/gjcase/templates/home/dj/app/gjcase/shared/env.sh
+++ itamae/roles/gjcase/templates/home/dj/app/gjcase/shared/env.sh
@@ -13,10 +13,10 @@ export SECRET_KEY_BASE="<%= node[:secrets][:'gjcase-secret_key_base'] %>"
 export DATABASE_URL="mysql2://gjcase:<%= node[:secrets][:'db-gjcase-password'] %>@$(cat ~/djbu-config/gj-mysql-master):3306/gjcase?encoding=utf8mb4&read_timeout=5&pool=5"
 export REDIS_URL="redis://$(cat ~/djbu-config/gj-redis-master):6379"
 
-export GJCASE_CAMO_URL=http://camo.kosendj-bu.in
+export GJCASE_CAMO_URL=https://camo.kosendj-bu.in
 export GJCASE_CAMO_KEY="<%= node[:secrets][:camo_key] %>"
 
-export GJCASE_STORAGE=http://gjcase-storage.kosendj-bu.in
+export GJCASE_STORAGE=https://gjcase-storage.kosendj-bu.in
 export GJCASE_S3_BUCKET=gjcase-prod
 export GJCASE_S3_REGION=ap-northeast-1
 
@@ -24,6 +24,6 @@ export GJCASE_AUTO_MIRROR=1
 
 export GJCASE_TUMBLR_KEY="<%= node[:secrets][:'gjcase-tumblr_consumer_key'] %>"
 
-export GJCASE_GJ_URL=http://gj.kosendj-bu.in
+export GJCASE_GJ_URL=https://gj.kosendj-bu.in
 
 exec "$@"
